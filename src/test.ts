18a94112e4403eb4098859e999900b714904a0bd
alwaysulf <ulfbaeltdjur@gmail.com>
2022-06-04 16:14:09 +0200

    Updates

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,7 @@
 const { Octokit } = require("@octokit/core");
+const readline = require("readline");
+const yaml = require("js-yaml");
+const fs = require("fs");
 console.log("Hello world");
 
 const dotenv = require("dotenv");
@@ -17,6 +20,7 @@ const getRepos = async () => {
   console.log(urls);
   const names = resp.data.map((repo) => repo.name);
   console.log(names);
+  let reposToSync = {};
   for (let name of names) {
     try {
       const resp = await octokit.request(
@@ -24,6 +28,16 @@ const getRepos = async () => {
         { repo: name }
       );
       console.log(resp);
+      const content = Buffer.from(resp.data.content, "base64").toString();
+      console.log(content);
+      const processedContent = content.trim().toLowerCase();
+
+      if (!processedContent.startsWith("#enablesync")) {
+        reposToSync["alwaysblind/" + name] = ["syncable-file"];
+        console.log("Will not sync file");
+        continue;
+      }
+      reposToSync["alwaysblind/" + name] = ["syncable-file"];
     } catch (e) {
       if (e.status === 404) {
         console.log(`Could not find file in repo ${name} 404`);
@@ -32,6 +46,16 @@ const getRepos = async () => {
       }
     }
   }
+  const reposYaml = yaml.dump(reposToSync);
+  const fileName = ".github/sync.yml";
+  fs.writeFile(fileName, reposYaml, (err) => {
+    if (err) {
+      console.log("Error while writing file");
+    } else {
+      console.log("successfully wrote file");
+      console.log(fs.readFileSync(fileName, "utf8"));
+    }
+  });
 };
 
 getRepos();
