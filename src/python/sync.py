795ec522d3b016b2e5f0b52f34eea8f7b91813d1
alwaysulf <ulfbaeltdjur@gmail.com>
2022-06-05 17:57:36 +0200

    test python action

---
+++ src/python/sync.py
@@ -0,0 +1,9 @@
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+# Select your transport with a defined url endpoint
+transport = AIOHTTPTransport(url="https://countries.trevorblades.com/")
+
+# Create a GraphQL client using the defined transport
+client = Client(transport=transport, fetch_schema_from_transport=True)
+print("Hej varld")
