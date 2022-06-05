bf5af39fea7bf707017f5071071b5b178ff875e6
alwaysulf <ulfbaeltdjur@gmail.com>
2022-06-05 16:23:53 +0200

    Reorganize

--- src/javascript/graphql.ts
+++ src/javascript/graphql.ts
@@ -1,17 +1,16 @@
-const { Octokit } = require("@octokit/core");
-const readline = require("readline");
-const yaml = require("js-yaml");
-const fs = require("fs");
+import { Octokit } from "@octokit/core";
+import yaml from "js-yaml";
+import fs from "fs";
 console.log("Hello world");
 
-const dotenv = require("dotenv");
+import dotenv from "dotenv";
 dotenv.config();
 const token = process.env.GH_PAT || process.env.GITHUB_TOKEN;
 console.log(token);
 
 const octokit = new Octokit({ auth: token });
 
-let { graphql, GraphqlResponseError } = require("@octokit/graphql");
+let { graphql } = require("@octokit/graphql");
 
 const main = async () => {
   graphql = graphql.defaults({
