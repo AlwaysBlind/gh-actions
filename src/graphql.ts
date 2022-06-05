9536e5a9fb86a2461a8f30d874b9d6fcdf43be4a
alwaysulf <ulfbaeltdjur@gmail.com>
2022-06-05 13:21:12 +0200

    Add new script using gql

---
+++ src/graphql.ts
@@ -0,0 +1,94 @@
+const { Octokit } = require("@octokit/core");
+const readline = require("readline");
+const yaml = require("js-yaml");
+const fs = require("fs");
+console.log("Hello world");
+
+const dotenv = require("dotenv");
+dotenv.config();
+const token = process.env.GH_PAT || process.env.GITHUB_TOKEN;
+console.log(token);
+
+const octokit = new Octokit({ auth: token });
+
+let { graphql, GraphqlResponseError } = require("@octokit/graphql");
+
+const main = async () => {
+  graphql = graphql.defaults({
+    headers: {
+      authorization: `token ${token}`,
+    },
+  });
+
+  async function fetchData() {
+    try {
+      const result = await graphql(query);
+      return result.repositoryOwner.repositories.edges.map(
+        (edge) => edge.repository
+      );
+    } catch (error) {
+      console.log("ERROR while fetching data from GitHub: ", error);
+    }
+  }
+
+  const repos = await fetchData();
+  if (repos) {
+    const syncObject = getSyncObject(repos);
+    dumpYaml(syncObject);
+  }
+};
+
+const query = `query {
+  repositoryOwner(login: "alwaysblind") {
+    repositories(first: 100) {
+      totalCount
+      edges {
+        repository: node {
+          name
+          file1: object(expression: "HEAD:syncable-file") {
+            id
+            ... on Blob {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+main();
+
+function getSyncObject(repos) {
+  return repos.reduce((acc, repo) => {
+    console.log(repo);
+    if (shouldSync(repo)) {
+      return { ...acc, ["alwaysblind/" + repo.name]: ["syncable-file"] };
+    }
+    return acc;
+  }, {});
+  //   let syncObject = {};
+  //   repos.forEach((repo) => {
+  //     if (shouldSync(repo)) {
+  //       syncObject[repo.name] = ["syncable-file"];
+  //     }
+  //   });
+  //   return syncObject;
+}
+
+const shouldSync = (repo) => {
+  return repo.file1?.text.startsWith("#enable-sync");
+};
+
+const dumpYaml = (reposToSync) => {
+  const reposYaml = yaml.dump(reposToSync);
+  const fileName = ".github/sync.yml";
+  fs.writeFile(fileName, reposYaml, (err) => {
+    if (err) {
+      console.log("Error while writing file");
+    } else {
+      console.log("successfully wrote file");
+      console.log(fs.readFileSync(fileName, "utf8"));
+    }
+  });
+};
--- src/test.ts
+++ src/test.ts
@@ -11,41 +11,51 @@ console.log(token);
 
 const octokit = new Octokit({ auth: token });
 
-const getRepos = async () => {
+const getRepoNames = async () => {
   const resp = await octokit.request("GET /user/repos", {
     user: "alwaysblind",
   });
-  console.log(resp);
-  const urls = resp.data.map((repo) => repo.url);
-  console.log(urls);
   const names = resp.data.map((repo) => repo.name);
-  console.log(names);
-  let reposToSync = {};
-  for (let name of names) {
-    try {
-      const resp = await octokit.request(
-        "GET /repos/alwaysblind/{repo}/contents/syncable-file",
-        { repo: name }
-      );
-      console.log(resp);
-      const content = Buffer.from(resp.data.content, "base64").toString();
-      console.log(content);
-      const processedContent = content.trim().toLowerCase();
+  return names;
+};
 
-      if (!processedContent.startsWith("#enablesync")) {
-        reposToSync["alwaysblind/" + name] = ["syncable-file"];
-        console.log("Will not sync file");
-        continue;
-      }
-      reposToSync["alwaysblind/" + name] = ["syncable-file"];
-    } catch (e) {
-      if (e.status === 404) {
-        console.log(`Could not find file in repo ${name} 404`);
-      } else {
-        console.log("Unexpected error");
-      }
+const getContent = async (repoName) => {
+  try {
+    const resp = await octokit.request(
+      "GET /repos/alwaysblind/{repo}/contents/syncable-file",
+      { repo: repoName }
+    );
+    const content = Buffer.from(resp.data.content, "base64").toString();
+    return content;
+  } catch (e) {
+    if (e.status === 404) {
+      console.log(`Could not find file in repo ${repoName} 404`);
+    } else {
+      console.log("Unexpected error");
     }
   }
+};
+
+const shouldSync = (content) => {
+  if (!content) {
+    return false;
+  }
+  const processedContent = content.trim().toLowerCase();
+  return !processedContent.startsWith("#enablesync");
+};
+
+const getSyncObject = async (names) => {
+  let reposToSync = {};
+  for (let name of names) {
+    const content = await getContent(name);
+    if (shouldSync(content)) {
+      reposToSync[name] = ["syncable-file"];
+    }
+  }
+  return reposToSync;
+};
+
+const dumpYaml = (reposToSync) => {
   const reposYaml = yaml.dump(reposToSync);
   const fileName = ".github/sync.yml";
   fs.writeFile(fileName, reposYaml, (err) => {
@@ -58,4 +68,10 @@ const getRepos = async () => {
   });
 };
 
-getRepos();
+const writeSyncFile = async () => {
+  const names = await getRepoNames();
+  const syncObject = await getSyncObject(names);
+  dumpYaml(syncObject);
+};
+
+writeSyncFile();
